85ec8ca09b946ed663878b880c15c80313498ad7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-10-22 18:23:58 +0700

    update

---
+++ 34-aaaa/code/mongoShell.js
@@ -0,0 +1,36 @@
+/*
+db.products.insertOne({
+    name: 'iphone X',
+    quantity: 120,
+    size: {width: 2.79, height: 5.65}
+})
+*/
+db.products.find({})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
