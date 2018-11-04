40f27a7abca05763895155f59fa4332fb337f08c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-04 21:08:59 +0700

    update

--- 52-aaaa/code/nodeappTemp/database/models/BlogPost.js
+++ 52-aaaa/code/nodeappTemp/database/models/BlogPost.js
@@ -34,8 +34,54 @@ const insertBlogPost = async (title, content, tokenKey) => {
         throw error
     }
 }
+const queryBlogPosts = async (text) => {
+    try {
+        let blogPosts = await BlogPost.find({
+            $or: [
+                {
+                    title: new RegExp(text, "i")//i => ko phân biệt hoa/thường
+                },
+                {
+                    content: new RegExp(text, "i")//i => ko phân biệt hoa/thường
+                }
+            ],            
+        })
+        return blogPosts
+    } catch(error) {        
+        throw error
+    }
+}
+const getDetailBlogPost = async (blogPostId) => {
+    try {
+        let blogPost = await BlogPost.findById(blogPostId)
+        if (!blogPost) {
+            throw `Không tìm thấy blogpost với Id=${blogPostId}`
+        }
+        return blogpost
+    } catch(error) {        
+        throw error
+    }
+}
 
 module.exports = {
 	BlogPost,
 	insertBlogPost
 }
+/*
+Chạy quảng cáo Facebook
+Để chạy quảng cáo Fb, bạn phải lập Fanpage, tạo tài khoản, viết bài quảng cáo, boost bài post
+
+Mua ô tô cũ, nên hay không
+Một số kinh nghiệm khi mua ô tô cũ,Những sự cố có thể gặp phải khi sử dụng xe cũ
+
+Ứng xử đẹp khi xảy ra va chạm giao thông
+Giữ cho bản thân và phương tiện an toàn khi tham gia giao thông là mục tiêu tất cả chúng ta đều hướng tới, tuy nhiên trên thực tế khi điều khiển phương tiện di chuyển sẽ có rất nhiều tình huống không lường trước được xảy ra. Lỗi có thể do nguyên nhân chủ quan hoặc khách quan và kết quả là va chạm
+
+Có nên mua xe Piaggio medley cũ qua mạng?
+Là một mẫu xe đắt tiền nên không phải ai cũng có có đủ tài chính để mua 1 chiếc xe mới nên lựa chọn mua xe piaggio medley cũ là một giải pháp để giảm bớt gánh nặng về tài chính. 
+
+Khi nào phải bật xi nhan?
+Trong khi chuyển hướng, người lái xe, người điều khiển xe máy chuyên dùng phải nhường quyền đi trước cho người đi bộ.
+
+
+*/
