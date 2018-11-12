81d9e5f8e9f194b38550069fbc9aaa2b6734a29c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-12 12:13:43 +0700

    aa

---
+++ 577-Tạo mới dự án Vuejs và làm quen với khái niệm components, props/code/vueapp/src/components/ComponentB.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>   
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'ComponentB',
+  //props = "Thuộc tính public"
+  props: {
+    message: String
+  }
+}
+</script>
+
+<!-- scoped: Chỉ có tác dụng trong file .vue này -->
+<style scoped>
+h1 {
+  color: green;  
+}
+</style>
