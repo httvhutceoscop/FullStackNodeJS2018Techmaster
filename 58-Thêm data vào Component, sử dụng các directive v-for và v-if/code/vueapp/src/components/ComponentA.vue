73e899432f2cb9dfecce4f394406712af0ec0ce7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-12 16:41:27 +0700

    update

--- 58-Thêm data vào Component, sử dụng các directive v-for và v-if/code/vueapp/src/components/ComponentA.vue
+++ 58-Thêm data vào Component, sử dụng các directive v-for và v-if/code/vueapp/src/components/ComponentA.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="hello">
-    <h1>{{ message }}</h1>   
-    <p>{{this.currentDateTime}}</p>
-    <p v-if="isShow">This is a show/hide text</p>
-    <h3>Đây là danh sách các blogpost</h3>
+    <h1>{{ message }}</h1> 
+    <p>{{this.currentDateTime}}</p>    
+    <p v-if="isShow">Dòng này sẽ ẩn / hiện</p>
+    <!-- v-if là 1 "directive": v-if = true thì thẻ chứa nó mới hiện ra -->
+    <!-- Đưa danh sách các blogPosts data vào 1 "list" -->
     <ol>
       <li v-for="blogpost in blogPosts" v-bind:key="blogpost.title">
-        <p class='bold'>{{blogpost.title}}</p>
+        <p class="bold">{{blogpost.title}}</p>
         <p>{{blogpost.content}}</p>
       </li>
     </ol>
@@ -18,21 +19,22 @@ export default {
   name: 'ComponentA',
   //props = "Thuộc tính public"
   props: {
-    message: String,    
+    message: String
   },
+  //Hàm data chứa các thuộc tính "private" của 1 Component
   data() {
     return {
-      //data = "thuộc tính private" của 1 component
-      //data = "state" trong React
-      currentDateTime:`Current date/time is :${new Date().toLocaleTimeString()}`,
-      //boolean value
-      isShow: true,
+      //Các thuộc tính "private" => Giống "state" trong React ! 
+      currentDateTime: `Thời gian hiện tại: ${new Date().toLocaleTimeString()}`,  
+      //data có thể là kiểu boolean
+      isShow: false,
+      //data là "mảng các object"
       blogPosts: [
         { 
           title: 'Học Vuejs để làm gì',
           content:'Vue dùng để viết giao diện web phía Front-end, từ Vue có thể tương tác với server Nodejs(back-end)'
          },
-        { 
+         { 
           title: 'React hay Vuejs giống nhau điểm gì',
           content:'Cả 2 đều viết bằng Javascript, đều dùng để viết Web front-end'
          },
@@ -40,7 +42,7 @@ export default {
           title: 'Tương tác giữa Vue và Nodejs',
           content:'Vue gửi các Request lên Nodejs để CRUD dữ liệu trên server, qua axios hoặc fetch'
          },
-    ]
+      ]
     }
   }
 }
@@ -52,6 +54,6 @@ h1 {
   color: red;  
 }
 p.bold {
-    font-weight: bold;
+  font-weight: bold;
 }
 </style>
