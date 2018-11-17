f52c1bc62e72476be35db66c6d812f19a84bebb0
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-17 16:11:50 +0700

    update

--- 61-aaaa/code/vueappTemp/src/components/Login.vue
+++ 61-aaaa/code/vueappTemp/src/components/Login.vue
@@ -1,27 +1,47 @@
+<!-- 
+  Khoá học FullStackNodejs Techmaster 2018
+ * Instructor: Nguyễn Đức Hoàng
+ * Youtube: https://www.youtube.com/c/nguyenduchoang
+ * Form đăng nhập 
+ yarn add vee-validate
+ -->
 <template>
   <div class="container">      
       <div class="row">
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
           <h5 class="text-center">Login to your account</h5>            
-          <form>
+          <form @submit.prevent="submit">
             <div class="form-group">
-              <input type="email" id="inputEmail" class="form-control" 
-                  placeholder="Email address" required autofocus>                  
-            </div>
+              <input type="text" id="inputEmail"
+                  v-model="email"
+                  v-validate="'required|email'"
+                  name="email"
+                  :class="{'form-control': true, 'border-danger': errors.has('email') }"
+                  placeholder="Email address" autofocus>                  
+              <i v-show="errors.has('email')" class="fa fa-warning"></i>
+              <span v-show="errors.has('email')" class="text-danger">{{ errors.first('email') }}</span>
+            </div>            
             <div class="form-group">
-              <input type="password" class="form-control" placeholder="Password" required>                
+              <input type="password" 
+                class="form-control" placeholder="Password" 
+                name="password"
+                v-validate="'required'">                
             </div>
+            <span v-show="errors.has('required')">{{ errors.first('required') }}</span>
             <div class="custom-control custom-checkbox mb-3">
                 <input type="checkbox" class="custom-control-input" id="customCheck1">
                 <label class="custom-control-label" for="customCheck1">Remember password</label>
             </div>
-            <button class="btn-lg btn-primary btn-block text-uppercase" type="submit">
+            <button class="btn-lg btn-primary btn-block text-uppercase" 
+              type="submit">
               Login
             </button>
             <hr class="my-4">
-            <button class="btn-lg btn-google btn-block text-uppercase" type="submit">
+            <button class="btn-lg btn-google btn-block text-uppercase" 
+              type="submit">
               <i class="fab fa-google mr-2"></i> Sign in with Google</button>
-            <button class="btn-lg btn-facebook btn-block text-uppercase" type="submit">
+            <button class="btn-lg btn-facebook btn-block text-uppercase" 
+              type="submit">
               <i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
           </form>
         </div>
@@ -30,6 +50,7 @@
 </template>
 
 <script>
+// import { Validator } from "vee-validate"
 export default {
   name: "Login",
   //props = "Thuộc tính public"
@@ -44,7 +65,28 @@ export default {
   },
 
   //Các phương thức "private"
-  methods: {}
+  methods: {
+    validateBeforeSubmit(e) {
+      alert(`e = ${JSON.stringify(e)}`)
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          // eslint-disable-next-line
+          alert('Form Submitted!');
+          return;
+        }  
+      })
+    },
+    
+    // login() {
+    //   alert('Bạn bấm đăng nhập email/password')
+    // },
+    // loginFacebook() {
+    //   alert('Bạn bấm đăng nhập Facebook')
+    // },
+    // loginGoogle() {
+    //   alert('Bạn bấm đăng nhập Google')
+    // },
+  }
 };
 </script>
 
