9b152017a9d9475544339980a2ad99f162c24468
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-18 10:56:15 +0700

    update

--- 61-aaaa/code/vueapp/src/main.js
+++ 61-aaaa/code/vueapp/src/main.js
@@ -2,12 +2,12 @@
  * Khoá học FullStackNodejs Techmaster 2018
  * Instructor: Nguyễn Đức Hoàng
  * Youtube: https://www.youtube.com/c/nguyenduchoang
- * Bài này sẽ thêm thư viện Bootstrap vào VueApp
  */
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import VeeValidate from 'vee-validate'
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 new Vue({
